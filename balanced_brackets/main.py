192c274a167a6aa12b4e18b447b66dfdeaa2f51f
Pujan Thapa <iampujan@outlook.com>
2020-12-27 08:04:08 +0545

    Create main.py

---
+++ balanced_brackets/main.py
@@ -0,0 +1,46 @@
+from stack import Stack 
+
+def is_match(p1,p2):
+  if p1 == "(" and p2 == ")":
+    return True
+  elif p1 == "{" and p2 == "}":
+    return True
+  elif p1== "[" and p2 == "]":
+    return True
+  else:
+    return False
+
+def is_paren_balanced(paren_string):
+  s = Stack()
+  index = 0
+  is_balanced = True
+
+  while index < len(paren_string) and is_balanced:
+    paren = paren_string[index]
+    if paren in "({[":
+      s.push(paren)
+    else:
+      if s.is_empty():
+        is_balanced = False
+      else:
+        top = s.pop()
+        if not is_match(top,paren):
+          is_balanced = False
+    index += 1
+
+  if s.is_empty() and is_balanced:
+    return True
+  else:
+    return False
+
+print("String : (((({})))) Balanced or not?")
+print(is_paren_balanced("(((({}))))"))
+
+print("String : [][]]] Balanced or not?")
+print(is_paren_balanced("[][]]]"))
+
+print("String : [][] Balanced or not?")
+print(is_paren_balanced("[][]"))
+
+a = input("Enter braces only: ")
+print(is_paren_balanced(a))
